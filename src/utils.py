af4a82dd7b6cd3f94661d497a45de9f5b1c28407
배한준 <hbae0830@mju.ac.kr>
2025-06-28 10:28:51 +0900

    feat : seed 함수 추가

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import random
+import numpy as np
+import torch
 
 def create_sample_submission(dataset_query, predictions, file_name='sample_submission.csv'):
     df = pd.DataFrame({
@@ -6,3 +9,12 @@ def create_sample_submission(dataset_query, predictions, file_name='sample_submi
         'identity': predictions
     })
     df.to_csv(file_name, index=False)
+    
+    
+def set_seed(seed=42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
